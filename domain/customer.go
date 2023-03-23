054dd2afbb0e8674ab599729016cc87030610ab7
Sonu Sonu <ssonu@paloaltonetworks.com>
2023-03-23 11:49:53 +0530

    DTO introduced

--- domain/customer.go
+++ domain/customer.go
@@ -1,6 +1,9 @@
 package domain
 
-import appError "github.com/Sonu875/goLearning/Errors"
+import (
+	appError "github.com/Sonu875/goLearning/Errors"
+	"github.com/Sonu875/goLearning/dto"
+)
 
 type Customer struct {
 	Id          string `db:"customer_id"`
@@ -14,4 +17,23 @@ type Customer struct {
 type CustomerRepo interface {
 	FindAll(status string) ([]Customer, *appError.AppError)
 	GetCustomerByID(id string) (*Customer, *appError.AppError)
+}
+
+func (c Customer) ToDto() dto.CustomerResponse {
+	status := ""
+	if c.Status == "0" {
+		status = "inactive"
+	} else {
+		status = "active"
+	}
+
+	return dto.CustomerResponse{
+		Id:          c.Id,
+		Name:        c.Name,
+		City:        c.City,
+		Zipcode:     c.Zipcode,
+		DateOfBirth: c.DateOfBirth,
+		Status:      status,
+	}
+
 }
--- service/customerService.go
+++ service/customerService.go
@@ -3,11 +3,12 @@ package service
 import (
 	appError "github.com/Sonu875/goLearning/Errors"
 	"github.com/Sonu875/goLearning/domain"
+	"github.com/Sonu875/goLearning/dto"
 )
 
 type CustomerService interface {
 	GetAllCustomer(status string) ([]domain.Customer, *appError.AppError)
-	GetCustomerByID(id string) (*domain.Customer, *appError.AppError)
+	GetCustomerByID(id string) (*dto.CustomerResponse, *appError.AppError)
 }
 
 type DefaultCustomerService struct {
@@ -16,10 +17,17 @@ type DefaultCustomerService struct {
 
 func (s DefaultCustomerService) GetAllCustomer(status string) ([]domain.Customer, *appError.AppError) {
 	return s.repo.FindAll(status)
+
 }
 
-func (s DefaultCustomerService) GetCustomerByID(id string) (*domain.Customer, *appError.AppError) {
-	return s.repo.GetCustomerByID(id)
+func (s DefaultCustomerService) GetCustomerByID(id string) (*dto.CustomerResponse, *appError.AppError) {
+
+	c, err := s.repo.GetCustomerByID(id)
+	if err != nil {
+		return nil, err
+	}
+	response := c.ToDto()
+	return &response, nil
 }
 
 func NewCustomerService(repo domain.CustomerRepo) DefaultCustomerService {
